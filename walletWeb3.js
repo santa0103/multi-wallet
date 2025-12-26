261aa971724de4f18409a1fe06054448dd1fbdd7
takumi <takumisakamoto0103@gmail.com>
2025-12-26 02:33:13 -0800

    Update walletWeb3.js

--- walletWeb3.js
+++ walletWeb3.js
@@ -1,12 +1,50 @@
-const Web3 = require('web3');
+if (typeof window.ethereum !== 'undefined') {
+    const web3 = new Web3(window.ethereum);
+    let userAddress;
 
-// Create an instance of the web3.js library using the network address
-const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/YOUR-PROJECT-ID"));
+    async function connectWallet() {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        userAddress = accounts[0];
+        console.log('Connected wallet address:', userAddress);
+    }
 
-// Connect to wallet
-async function connectToWallet(privateKey) {
-   const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-   web3.eth.accounts.wallet.add(account);
-   web3.eth.defaultAccount = account.address;
-   return account.address;
+    async function getBalance(address) {
+        const tokenAddress = '0xYourTokenAddress'; // آدرس قرارداد توکن
+        const abi = [
+            {
+                "constant": true,
+                "inputs": [{"name": "_owner", "type": "address"}],
+                "name": "balanceOf",
+                "outputs": [{"name": "balance", "type": "uint256"}],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            }
+        ];
+        const contract = new web3.eth.Contract(abi, tokenAddress);
+        const balance = await contract.methods.balanceOf(address).call();
+        return web3.utils.fromWei(balance);
+    }
+
+    async function sendTokens(to, amount) {
+        const tokenAddress = '0xYourTokenAddress'; // آدرس قرارداد توکن
+        const abi = [
+            {
+                "constant": false,
+                "inputs": [{"name": "to", "type": "address"}, {"name": "amount", "type": "uint256"}],
+                "name": "transfer",
+                "outputs": [{"name": "", "type": "bool"}],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            }
+        ];
+        const contract = new web3.eth.Contract(abi, tokenAddress);
+
+        await contract.methods.transfer(to, web3.utils.toWei(amount, 'ether')).send({ from: userAddress });
+    }
+
+    connectWallet();
+} else {
+    alert('Please install MetaMask!');
 }
