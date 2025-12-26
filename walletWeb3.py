6f538a4a7a0d7c65ad0b73b0375ba2df38debdc9
takumi <takumisakamoto0103@gmail.com>
2025-12-26 02:33:48 -0800

    Update walletWeb3.py

--- walletWeb3.py
+++ walletWeb3.py
@@ -1,24 +1,30 @@
-from web3 import Web3, contract
+from web3 import Web3
 
-# Connect to the Ethereum network
-w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/your-project-id'))
+# اتصال به شبکه Rinkeby یا هر شبکه دیگری
+w3 = Web3(Web3.HTTPProvider('https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID'))
 
-# Address of the first contract
-wallet_address = "0x123456789abcdef..."
+# آدرس قرارداد توکن
+token_address = '0xYourTokenAddress'
+abi = [
+    {
+        "constant": True,
+        "inputs": [{"name": "_owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
 
-# ABI address of the first contract
-wallet_abi = [...] # ABI The first contract
+# اتصال به قرارداد
+contract = w3.eth.contract(address=token_address, abi=abi)
 
-# Contract The first contract
-wallet_contract = w3.eth.contract(address=wallet_address, abi=wallet_abi)
+# تابع برای دریافت موجودی
+def get_balance(address):
+    return contract.functions.balanceOf(address).call()
 
-# Connection function
-def connect_wallet(token_address):
-    #  Contract The second contract
-    token_contract = w3.eth.contract(address=token_address, abi=token_abi)
-
-# Add the first contract to the second contract
-token_contract.functions.setWalletContract(wallet_address).transact({'from': w3.eth.accounts[0]})
-
-# Return the Contract object of the second contract for use in the rest of the functions
-return token_contract
+# آدرس کیف پول
+address = '0xYourWalletAddress'
+balance = get_balance(address)
+print(f'Balance: {balance / 10**18} Tokens')  # موجودی به واحد اصلی
